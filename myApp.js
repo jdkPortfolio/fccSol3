bb918e9c1bf5c24622f987ad65d6cba35bff23bd
duncanjohanne <duncankachasu@gmail.com>
2023-08-04 17:10:35 +0200

    08041710

--- myApp.js
+++ myApp.js
@@ -5,6 +5,21 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 let Person;
 
+const personSchema = new mongoose.Schema({
+  name : {
+    type:String,
+    required:true
+  },
+  age: {
+    type:Number
+  },
+  favoriteFoods: {
+    type:[String]
+  }
+});
+
+Person = mongoose.model("Person", personSchema);
+
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
